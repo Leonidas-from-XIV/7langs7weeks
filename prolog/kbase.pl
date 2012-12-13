fc4eee946de66fe449498360e8f7b2cedf33dc1e
Marek Kubica <marek@xivilization.net>
2012-12-14 00:17:03 +0100

    Finished day one tasks for Prolog

---
+++ prolog/kbase.pl
@@ -0,0 +1,51 @@
+author(clarke).
+author(stephenson).
+author(martin).
+author(palahniuk).
+author(brown).
+author(forsyth).
+author(schaetzing).
+
+written_by(rendezvous_with_rama, clarke).
+written_by(space_odyssey, clarke).
+written_by(snow_crash, stephenson).
+written_by(diamond_age, stephenson).
+written_by(cryptonomicon, stephenson).
+written_by(game_of_thrones, martin).
+written_by(clash_of_kings, martin).
+written_by(storm_of_swords, martin).
+written_by(feast_for_crows, martin).
+written_by(dance_with_dragons, martin).
+written_by(fight_club, palahniuk).
+written_by(pygmy, palahniuk).
+written_by(angels_and_demons, brown).
+written_by(digital_fortress, brown).
+written_by(dogs_of_war, forsyth).
+written_by(negotiator, forsyth).
+written_by(dunkle_seite, schaetzing).
+written_by(limit, schaetzing).
+written_by(schwarm, schaetzing).
+
+% written_by(X, clarke).
+
+musician(oldfield).
+musician(groehl).
+musician(keenan).
+musician(carey).
+musician(jones).
+musician(chancellor).
+
+genre(keenan, progrock).
+genre(carey, progrock).
+genre(jones, progrock).
+genre(chancellor, progrock).
+genre(groehl, altrock).
+
+plays(oldfield, guitar).
+plays(oldfield, tubular_bells).
+plays(groehl, drums).
+plays(jones, guitar).
+plays(carey, drums).
+plays(chancellor, bass).
+
+% plays(X, guitar).
