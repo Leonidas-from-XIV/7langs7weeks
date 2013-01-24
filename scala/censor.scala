fe02d6bf2eb4a8ede2003f1f8b451c2b705db23a
Marek Kubica <marek@xivilization.net>
2013-01-24 15:47:05 +0100

    Finished day 2 Scala homework

--- scala/censor.scala
+++ scala/censor.scala
@@ -1,8 +1,16 @@
+import scala.util.parsing.json.JSON
+import scala.io.Source
+
 trait Censor {
-  val mapping = prepare(Map(
-    "shoot" -> "pucky",
-    "darn" -> "beans"))
-  def prepare(map: Map[String, String]) =
+  val src = Source.fromFile("censor.json").mkString
+  val loaded = JSON.parseFull(src)
+  val parsed = loaded match {
+    case Some(e) => e.asInstanceOf[Map[String,String]]
+    case None => Map[String,String]()
+  }
+  val mapping = casePrepare(parsed)
+
+  def casePrepare(map: Map[String, String]) =
     map.foldLeft(map){(res, entry) =>
       val (k, v) = entry
       res + (k.capitalize -> k.capitalize)
@@ -21,3 +29,4 @@ class AmericanMovie extends Movie with Censor
 
 val am = new AmericanMovie()
 println(am.censor("Pretty darn irresistible. Well, shoot."))
+//println(am.mapping)
