42dd79134d60dbcc18ac2718319b80fc6e80d43d
Marek Kubica <marek@xivilization.net>
2013-02-01 23:35:41 +0100

    Added solution to sleeping barber problem

--- clojure/day3.clj
+++ clojure/day3.clj
@@ -22,3 +22,49 @@
 (println (dosync (add-account accounts "Leonidas")))
 (println (dosync (credit accounts "Leonidas" 10.0)))
 (println (dosync (debit accounts "Leonidas" 5.0)))
+
+; barber <serviced>
+(def barber (agent 0))
+; seats <free>
+(def free-seats (agent 3))
+
+(defn any-seat-occupied? []
+  (< @free-seats 3))
+
+(defn free-seat [free]
+  (inc free))
+
+(defn try-occupy [free]
+  (if (= free 0) 0 (dec free)))
+
+(defn cut-hair [serviced]
+  (if (any-seat-occupied?)
+    (do
+      (send free-seats free-seat)
+      (Thread/sleep 20)
+      (inc serviced))
+    serviced))
+
+(def continue-running (atom true))
+
+(defn move-to-chair []
+  (loop []
+    (if @continue-running
+      (do
+        (send barber cut-hair)
+        (recur)))))
+
+(defn client-arrive []
+  (loop []
+    (if @continue-running
+      (let [time (+ 10 (rand-int 20))]
+        (Thread/sleep time)
+        (send free-seats try-occupy)
+        (recur)))))
+
+(.start (Thread. move-to-chair))
+(.start (Thread. client-arrive))
+(Thread/sleep (* 10 1000))
+(println @barber)
+(reset! continue-running false)
+(shutdown-agents)
