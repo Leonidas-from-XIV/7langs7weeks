501817e6b69b4a95d0af7a1fe07e364d66188018
Marek Kubica <marek@xivilization.net>
2013-02-08 17:24:30 +0100

    Implemented maze
    With help from Wakatta to get some sane definitions

--- haskell/day3.hs
+++ haskell/day3.hs
@@ -1,4 +1,13 @@
 module Main where
     hash = [(23, []), (42, [("lol", []), ("rofl", [("heh", 9001)])])]
 
-    getInner = Just hash >>= lookup 42 >>= lookup "rofl" >>= lookup "heh" >>= return
+    getInner = return hash >>= lookup 42 >>= lookup "rofl" >>= lookup "heh"
+
+    data Node = Exits [(Integer,Integer)] deriving (Show)
+    data Maze = Maze [[Node]] deriving (Show)
+
+    myMaze = Maze [[Exits [(0,1)], Exits [(1,1), (0,2)], Exits []],
+                   [Exits [], Exits [(1,2)], Exits [(2,2)]],
+                   [Exits [], Exits [], Exits []]]
+
+    nodeFromMaze maze (x,y) = maze !! x !! y
