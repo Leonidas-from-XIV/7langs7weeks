048589aa4f499cc7aa57455d0ce52ccd029a7ac9
Marek Kubica <marek@xivilization.net>
2013-02-09 20:27:38 +0100

    A somehow working solution for maze problem

--- haskell/day3.hs
+++ haskell/day3.hs
@@ -3,12 +3,12 @@ module Main where
 
     getInner = return hash >>= lookup 42 >>= lookup "rofl" >>= lookup "heh"
 
-    data Node = Exits [(Int,Int)] deriving (Show)
+    data Node = Exits Integer [(Int,Int)] deriving (Show, Eq)
     data Maze = Maze [[Node]] deriving (Show)
 
-    myMaze = Maze [[Exits [(0,1)], Exits [(1,1), (0,2)], Exits []],
-                   [Exits [], Exits [(1,2)], Exits [(2,2)]],
-                   [Exits [], Exits [], Exits []]]
+    myMaze = Maze [[Exits 1 [(0,1)], Exits 2 [(1,1), (0,2)], Exits 3 []],
+                   [Exits 4 [], Exits 5 [(1,2)], Exits 6 [(2,2)]],
+                   [Exits 6 [], Exits 8 [], Exits 9 []]]
 
     nodeFromMaze :: Maze -> (Int,Int) -> Node
     nodeFromMaze (Maze maze) (x,y) = maze !! x !! y
@@ -17,10 +17,22 @@ module Main where
     mazeNode = nodeFromMaze myMaze
 
     startNode = mazeNode (0,0)
-    endNode = mazeNode (2,2)
-
+    targetNode = mazeNode (2,2)
     startPath = [[startNode]]
+    checkIfEnd n = endNode == n
+
+    -- takes the last node and creates new paths with all possible exit nodes
     extendPath path = let end = last path
-                          Exits ways = end
+                          Exits _ ways = end
                           exitNodes = map mazeNode ways
                           in map (\n -> path ++ [n]) exitNodes
+
+    endNode = last
+    -- filter out valid solutions
+    completePaths path = filter (\p -> (endNode p) == targetNode) path
+    -- check if we found valid solutions, otherwise >>= extendPath
+    exploreMaze p = let complete = completePaths p
+                        found = complete /= []
+                        in if found then (head complete) else exploreMaze (p >>= extendPath)
+
+    solution = exploreMaze startPath
