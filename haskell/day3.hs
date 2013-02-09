7f82e5f93cd4407e4b82fa325cab429be79f2364
Marek Kubica <marek@xivilization.net>
2013-02-09 19:51:04 +0100

    Semi-solution to path finding homework

--- haskell/day3.hs
+++ haskell/day3.hs
@@ -3,11 +3,24 @@ module Main where
 
     getInner = return hash >>= lookup 42 >>= lookup "rofl" >>= lookup "heh"
 
-    data Node = Exits [(Integer,Integer)] deriving (Show)
+    data Node = Exits [(Int,Int)] deriving (Show)
     data Maze = Maze [[Node]] deriving (Show)
 
     myMaze = Maze [[Exits [(0,1)], Exits [(1,1), (0,2)], Exits []],
                    [Exits [], Exits [(1,2)], Exits [(2,2)]],
                    [Exits [], Exits [], Exits []]]
 
-    nodeFromMaze maze (x,y) = maze !! x !! y
+    nodeFromMaze :: Maze -> (Int,Int) -> Node
+    nodeFromMaze (Maze maze) (x,y) = maze !! x !! y
+
+    mazeNode :: (Int,Int) -> Node
+    mazeNode = nodeFromMaze myMaze
+
+    startNode = mazeNode (0,0)
+    endNode = mazeNode (2,2)
+
+    startPath = [[startNode]]
+    extendPath path = let end = last path
+                          Exits ways = end
+                          exitNodes = map mazeNode ways
+                          in map (\n -> path ++ [n]) exitNodes
